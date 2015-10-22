d33bb9af3eb75e247e63dcb02f5b98ee7de3e702
Victor Juliani <v_juliani@hotmail.com>
2015-10-22 12:19:37 -0200

    Fixed some tests
    Pickup em todos os testes, pois o setup é sempre reiniciado.
    Correção dos error range.
    Remoção de teste de includes depois do hangup.

--- src/telecom/telecomTesteFuncional.java
+++ src/telecom/telecomTesteFuncional.java
@@ -22,131 +22,150 @@ public class telecomTesteFuncional
 		call1 = new Call(c1, c2, false);
 		call2 = new Call(c1, c3, false);
 		call3 = new Call(c1, c3, true);
-		
 	}
 	
-	/* Teste Funcional 1: Ligacao local - verificar conexao e se os
-	 * customers corretos estao conectados*/
+	/*
+	 * Teste Funcional 1: Ligacao local - verificar conexao e se os
+	 * customers corretos estao conectados
+	 */
 	@Test
 	public void test01()
 	{
 		call1.pickup();
+		
 		assertTrue(call1.isConnected());
 		assertTrue(call1.includes(c1));
 		assertTrue(call1.includes(c2));
 		assertFalse(call1.includes(c3));
 	}
 	
-	/* Teste Funcional 2: Ligacao a distancia - verificar conexao e se os
-	 * customers corretos estao conectados*/
-	@Test 
+	/*
+	 * Teste Funcional 2: Ligacao a distancia - verificar conexao e se os
+	 * customers corretos estao conectados
+	 */
+	@Test
 	public void test02()
 	{
 		call2.pickup();
+		
 		assertTrue(call2.isConnected());
 		assertTrue(call2.includes(c1));
 		assertTrue(call2.includes(c3));
 		assertFalse(call2.includes(c2));
 	}
 	
-	/* Teste Funcional 3: Ligacao mobile a distancia - verificar conexao e se os
-	 * customers corretos estao conectados*/
+	/*
+	 * Teste Funcional 3: Ligacao mobile a distancia - verificar conexao e se os
+	 * customers corretos estao conectados
+	 */
 	@Test
 	public void test03()
 	{
 		call3.pickup();
+		
 		assertTrue(call3.isConnected());
 		assertTrue(call3.includes(c1));
 		assertTrue(call3.includes(c3));
 		assertFalse(call3.includes(c2));
 	}
 	
-	
-	/*Teste funcional 4: testando se uma excessao e lancada quando caller eh null*/
-	@Test(expected = Exception.class) 
+	/* Teste funcional 4: testando se uma excessao e lancada quando caller eh null */
+	@Test(expected = Exception.class)
 	public void test04() throws NullPointerException
 	{
 		call4 = new Call(null, c2, false);
 	}
 	
-	/*Teste funcional 5: testando se uma excessao e lancada quando receiver eh null*/
-	@Test(expected = Exception.class) 
+	/* Teste funcional 5: testando se uma excessao e lancada quando receiver eh null */
+	@Test(expected = Exception.class)
 	public void test05() throws NullPointerException
 	{
 		call5 = new Call(c1, null, false);
 	}
 	
-	/* Teste funcional 7: Verificar resultados apos hangup para ligacao local */
+	/* Teste funcional 6: Verificar resultados apos hangup para ligacao local */
 	@Test
-	public void test07() throws InterruptedException
+	public void test06() throws InterruptedException
 	{
+		call1.pickup();
 		int wait = 500;
 		
 		Thread.sleep(wait);
 		call1.hangup();
+		
 		assertFalse(call1.isConnected());
-		assertFalse(call1.includes(c1));
-		assertFalse(call1.includes(c2));
+		
 		Timing t = Timing.aspectOf();
 		Billing b = Billing.aspectOf();
+		
 		assertEquals(wait, t.getTotalConnectTime(c1), 50);
 		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c2));
 		assertEquals(0, b.getTotalCharge(c2));
 		assertEquals(wait * 3, b.getTotalCharge(c1), 150);
 	}
 	
-	/* Teste funcional 8: Verificar resultados apos hangup para ligacao longa distancia */
+	/* Teste funcional 7: Verificar resultados apos hangup para ligacao longa distancia */
 	@Test
-	public void test08() throws InterruptedException
+	public void test07() throws InterruptedException
 	{
-		call2 = new Call(c1, c3, false);
+		call2.pickup();
 		int wait = 500;
 		
 		Thread.sleep(wait);
 		call2.hangup();
+		
 		assertFalse(call2.isConnected());
+		
 		Timing t = Timing.aspectOf();
 		Billing b = Billing.aspectOf();
-		assertEquals(wait, t.getTotalConnectTime(c1), 550);
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 50);
 		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3));
 		assertEquals(0, b.getTotalCharge(c3));
-		assertEquals(wait * 10, b.getTotalCharge(c1), 650);
+		assertEquals(wait * 10, b.getTotalCharge(c1), 150);
 	}
 	
-	/* Teste funcional 9: Verificar resultados apos hangup para ligacao mobile longa distancia */
+	/* Teste funcional 8: Verificar resultados apos hangup para ligacao mobile longa distancia */
 	@Test
-	public void test09() throws InterruptedException
+	public void test08() throws InterruptedException
 	{
+		call3.pickup();
 		int wait = 500;
 		
 		Thread.sleep(wait);
 		call3.hangup();
+		
 		assertFalse(call3.isConnected());
+		
 		Timing t = Timing.aspectOf();
 		Billing b = Billing.aspectOf();
-		assertEquals(wait, t.getTotalConnectTime(c1), 1050);
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 50);
 		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3));
 		assertEquals(0, b.getTotalCharge(c3), 10);
-		assertEquals(wait * 10, b.getTotalCharge(c1), 1150);
+		assertEquals(wait * 10, b.getTotalCharge(c1), 150);
 	}
 	
-	/* Teste funcional 10: Verificar resultados apos merge das tres ligacoes */
+	/* Teste funcional 9: Verificar resultados apos merge das tres ligacoes */
 	@Test
-	public void test10() throws InterruptedException
+	public void test09() throws InterruptedException
 	{
-		call1.includes(c3);
-		call1.merge(call3);
+		call1.pickup();
+		call2.pickup();
 		int wait = 500;
 		
 		Thread.sleep(wait);
+		call1.merge(call2);
 		call1.hangup();
+		
 		Timing t = Timing.aspectOf();
 		Billing b = Billing.aspectOf();
-		assertEquals(wait, t.getTotalConnectTime(c1), 1050);
-		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c2));
-		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3));
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 20);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c2), 20);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3), 20);
 		assertEquals(0, b.getTotalCharge(c2));
 		assertEquals(0, b.getTotalCharge(c3));
-		assertEquals(wait * 3, b.getTotalCharge(c1), 1150);
+		assertEquals((wait * 3) + (wait * 10), b.getTotalCharge(c1), 260);
 	}
 }
