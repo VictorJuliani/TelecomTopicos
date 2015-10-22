78e91cb1f5f650403901c03da6fbec222dc1c99c
Victor Juliani <v_juliani@hotmail.com>
2015-10-22 12:21:00 -0200

    Refactored tests to test folder

--- tests/telecom/FunctionalTestSet.java
+++ tests/telecom/FunctionalTestSet.java
@@ -1,93 +1,171 @@
 package telecom;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class FunctionalTestSet extends TestCase
+import org.junit.Before;
+import org.junit.Test;
+
+public class FunctionalTestSet
 {
+	static Customer c1, c2, c3;
+	static Call call1, call2, call3, call4, call5;
 	
-	// Functional testing the Call class
-	
-	public FunctionalTestSet(String name)
+	@Before
+	public void setup()
 	{
-		super(name);
+		c1 = new Customer("nome1", 12, "12343212");
+		c2 = new Customer("nome2", 12, "12343214");
+		c3 = new Customer("nome5", 16, "12643212");
+		
+		call1 = new Call(c1, c2, false);
+		call2 = new Call(c1, c3, false);
+		call3 = new Call(c1, c3, true);
 	}
 	
-	/**
-	 * Constructor specification:
-	 * Create a new call connecting caller to receiver
-	 * with a new connection. This should really only be
-	 * called by Customer.call(..)
+	/*
+	 * Teste Funcional 1: Ligacao local - verificar conexao e se os
+	 * customers corretos estao conectados
 	 */
-	
-	/**
-	 * Pickup specification:
-	 * picking up a call completes the current connection
-	 * (this means that you shouldnt merge calls until
-	 * they are completed)
-	 */
-	
-	public void testPickup()
+	@Test
+	public void test01()
 	{
-		Call c = new Call(new Customer("Jim", 650, "3361-1111"), new Customer("Mik", 500, "3361-1112"), false);
-		c.pickup();
-		assertTrue(c.isConnected());
+		call1.pickup();
+		
+		assertTrue(call1.isConnected());
+		assertTrue(call1.includes(c1));
+		assertTrue(call1.includes(c2));
+		assertFalse(call1.includes(c3));
 	}
 	
-	/**
-	 * Hangup specification:
-	 * hanging up a call drops the connection
+	/*
+	 * Teste Funcional 2: Ligacao a distancia - verificar conexao e se os
+	 * customers corretos estao conectados
 	 */
-	public void testHangup()
+	@Test
+	public void test02()
 	{
-		Customer jim = new Customer("Jim", 650, "3361-1111");
-		Customer mik = new Customer("Mik", 500, "3361-1112");
-		Call c = new Call(jim, mik, false);
-		c.pickup();
-		c.hangup();
-		assertFalse(c.isConnected());
+		call2.pickup();
+		
+		assertTrue(call2.isConnected());
+		assertTrue(call2.includes(c1));
+		assertTrue(call2.includes(c3));
+		assertFalse(call2.includes(c2));
 	}
 	
-	/**
-	 * Includes specification:
-	 * is Customer c one of the customers in this call?
+	/*
+	 * Teste Funcional 3: Ligacao mobile a distancia - verificar conexao e se os
+	 * customers corretos estao conectados
 	 */
-	
-	public void testIncludesTrue()
+	@Test
+	public void test03()
 	{
-		Customer jim = new Customer("Jim", 650, "3361-1111");
-		Customer mik = new Customer("Mik", 500, "3361-1112");
-		Call c = new Call(jim, mik, false);
-		c.pickup();
-		assertTrue(c.includes(mik));
+		call3.pickup();
+		
+		assertTrue(call3.isConnected());
+		assertTrue(call3.includes(c1));
+		assertTrue(call3.includes(c3));
+		assertFalse(call3.includes(c2));
 	}
 	
-	public void testIncludesFalse()
+	/* Teste funcional 4: testando se uma excessao e lancada quando caller eh null */
+	@Test(expected = Exception.class)
+	public void test04() throws NullPointerException
 	{
-		Customer jim = new Customer("Jim", 650, "3361-1111");
-		Customer mik = new Customer("Mik", 500, "3361-1112");
-		Customer john = new Customer("John", 500, "3361-1113");
-		Call c = new Call(jim, mik, false);
-		c.pickup();
-		assertFalse(c.includes(john));
+		call4 = new Call(null, c2, false);
 	}
 	
-	/**
-	 * Merge specification:
-	 * Merge all connections from call 'other' into 'this'
-	 */
-	
-	public void testMerge()
+	/* Teste funcional 5: testando se uma excessao e lancada quando receiver eh null */
+	@Test(expected = Exception.class)
+	public void test05() throws NullPointerException
 	{
-		Customer jim = new Customer("Jim", 650, "3361-1111");
-		Customer mik = new Customer("Mik", 500, "3361-1112");
-		Customer john = new Customer("John", 500, "3361-1113");
-		Customer cris = new Customer("Cris", 500, "3361-1114");
-		Call c1 = new Call(jim, mik, false);
-		c1.pickup();
-		Call c2 = new Call(john, cris, false);
-		c2.pickup();
-		c1.merge(c2);
-		assertTrue(c1.includes(john));
-		assertTrue(c1.includes(cris));
+		call5 = new Call(c1, null, false);
+	}
+	
+	/* Teste funcional 6: Verificar resultados apos hangup para ligacao local */
+	@Test
+	public void test06() throws InterruptedException
+	{
+		call1.pickup();
+		int wait = 500;
+		
+		Thread.sleep(wait);
+		call1.hangup();
+		
+		assertFalse(call1.isConnected());
+		
+		Timing t = Timing.aspectOf();
+		Billing b = Billing.aspectOf();
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 50);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c2));
+		assertEquals(0, b.getTotalCharge(c2));
+		assertEquals(wait * 3, b.getTotalCharge(c1), 150);
+	}
+	
+	/* Teste funcional 7: Verificar resultados apos hangup para ligacao longa distancia */
+	@Test
+	public void test07() throws InterruptedException
+	{
+		call2.pickup();
+		int wait = 500;
+		
+		Thread.sleep(wait);
+		call2.hangup();
+		
+		assertFalse(call2.isConnected());
+		
+		Timing t = Timing.aspectOf();
+		Billing b = Billing.aspectOf();
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 50);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3));
+		assertEquals(0, b.getTotalCharge(c3));
+		assertEquals(wait * 10, b.getTotalCharge(c1), 150);
+	}
+	
+	/* Teste funcional 8: Verificar resultados apos hangup para ligacao mobile longa distancia */
+	@Test
+	public void test08() throws InterruptedException
+	{
+		call3.pickup();
+		int wait = 500;
+		
+		Thread.sleep(wait);
+		call3.hangup();
+		
+		assertFalse(call3.isConnected());
+		
+		Timing t = Timing.aspectOf();
+		Billing b = Billing.aspectOf();
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 50);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3));
+		assertEquals(0, b.getTotalCharge(c3), 10);
+		assertEquals(wait * 10, b.getTotalCharge(c1), 150);
+	}
+	
+	/* Teste funcional 9: Verificar resultados apos merge das tres ligacoes */
+	@Test
+	public void test09() throws InterruptedException
+	{
+		call1.pickup();
+		call2.pickup();
+		int wait = 500;
+		
+		Thread.sleep(wait);
+		call1.merge(call2);
+		call1.hangup();
+		
+		Timing t = Timing.aspectOf();
+		Billing b = Billing.aspectOf();
+		
+		assertEquals(wait, t.getTotalConnectTime(c1), 20);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c2), 20);
+		assertEquals(t.getTotalConnectTime(c1), t.getTotalConnectTime(c3), 20);
+		assertEquals(0, b.getTotalCharge(c2));
+		assertEquals(0, b.getTotalCharge(c3));
+		assertEquals((wait * 3) + (wait * 10), b.getTotalCharge(c1), 260);
 	}
 }
